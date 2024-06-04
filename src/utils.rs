e528c74718beeadfffee1763b3ee3d8843a11106
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-06-03 22:37:08 -0600

    lua!!!

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ use dirs::{desktop_dir, picture_dir};
 
 pub static DECK_NAME : &str = "baraja";
 pub static BOARD_NAME : &str = "cartas";
-pub static INSTUCTIONS_NAME: &str = "loteria";
+pub static INSTUCTIONS_NAME: &str = "loteria.txt";
 
 pub trait Append<T> where Self: Sized
 { fn append(self, v: T) -> Self; }
@@ -18,29 +18,33 @@ impl<T> Append<T> for PathBuf where T : AsRef<Path> + Sized{
 }
 
 pub fn get_board_path(debug: bool) -> Result<PathBuf>{
-    let path = picture_dir().ok_or(anyhow!("Pictures not found!"))?;
+    let path = picture_dir()
+        .ok_or(anyhow!("Pictures not found!"))?
+        .append(BOARD_NAME);
     if debug{
         println!("using default board path {}", path.display());
     }
-    Ok(path.append(BOARD_NAME))
+    Ok(path)
 }
 
 pub fn get_deck_path(debug: bool) -> Result<PathBuf>{
-    let path = picture_dir().ok_or(anyhow!("Pictures not found!"))?;
+    let path = picture_dir()
+        .ok_or(anyhow!("Pictures not found!"))?
+        .append(DECK_NAME);
     if debug{
         println!("using default deck path {}", path.display());
     }
-    Ok(picture_dir().ok_or(anyhow!("deck path not found"))?.append(DECK_NAME))
+    Ok(path)
 }
 
 pub fn get_instruction_path(debug: bool) -> Result<PathBuf>{
-    let path = desktop_dir().ok_or(anyhow!("Desktop not found!"))?;
+    let path = desktop_dir()
+        .ok_or(anyhow!("Desktop not found!"))?
+        .append(INSTUCTIONS_NAME);
     if debug{
         println!("using default instruction path {}", path.display());
     }
-    let mut p = path.append(INSTUCTIONS_NAME);
-    p.set_extension("txt");
-    Ok(p)
+    Ok(path)
 }
 
 pub fn wait_enter(){
