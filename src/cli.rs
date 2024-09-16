147f31c4eff61a821fe4481bc886c8d1bf5b2d87
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-09-16 13:40:38 -0600

    log again

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,84 @@
 use std::{fs::File, io::{BufReader, Read}, path::Path};
 use anyhow::*;
+use image::{GenericImageView, RgbaImage};
+use loteria_engine::{init, run, BOARD};
+
+use crate::{debug, Opts};
 
 
 pub struct Cli{
     lua_src : String,
+
+    opts: Opts,
 }
 
 impl Cli{
     pub fn run(self) -> Result<()>{
+        init(self.lua_src.as_str())?;
+        let tables = run()?;
+        let (count, total, (board_w, board_h)) = unsafe{(
+            BOARD.get_count(),
+            BOARD.get_total(),
+            BOARD.get_dims(),
+        )};
+
+        let mut images = Vec::new();
+        for i in 0..count{
+            let mut path = self.opts.deck.clone();
+            path.push(format!("{}{i:03}", self.opts.in_format));
+            path.set_extension("png");
+            debug!(self.opts, "loading image {}", path.display());
+
+            let image = image::open(path)?;
+            images.push(image);
+
+        }
+
+        let (width, height) = images[0].dimensions();
+        let mut buffer = RgbaImage::new(width * board_w as u32, height * board_h as u32);
+
+        for indx in 0..total{
+            debug!(self.opts, "generating board {indx}..");
+            let table = &tables[indx];
+            for ij in 0..16{
+                let i = ij % 4;
+                let j = ij / 4;
+
+                let card_index = table.get(i, j).unpack();
+                for w in 0..width{
+                    for h in 0..height{
+                        let card_p = images[card_index].get_pixel(w as u32, h as u32);
+                        let buff_p = buffer.get_pixel_mut(i as u32 * width + w , j as u32 * height + h);
+                        *buff_p = card_p;
+                    }
+                }
+            }
+
+            let mut path = self.opts.output.clone();
+            path.push(format!("{}{indx:03}", self.opts.out_format));
+            path.set_extension("png");
+
+            debug!(self.opts, "saving at {}..", path.display());
+            buffer.save(path)?;
+        }
+
         Ok(())
     }
 
-    pub fn from_path<P: AsRef<Path>>(path: P) -> Result<Self>{
+    pub fn config<P: AsRef<Path>>(opts: Opts, path: P) -> Result<Self>{
+        let path = path.as_ref();
+        debug!(opts, "lua path: {}", path.display());
         let mut file = BufReader::new( File::open(path)? );
         let mut buf = Vec::new();
+        
+
         file.read_to_end(&mut buf)?;
         let lua_src = String::from_utf8(buf)?;
+        debug!(opts, "lua src:\n{lua_src}");
 
-        Ok(Self{ lua_src })
+        Ok(Self{
+            opts,
+            lua_src,
+        })
     }
 }
--- src/gui.rs
+++ src/gui.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::DataOpts;
+use crate::{cli::Cli, Opts};
 
 use anyhow::anyhow;
 use eframe::{egui, App};
@@ -13,28 +13,79 @@ pub struct GUI{
     error : String,
 
     #[allow(dead_code)]
-    opts: DataOpts,
+    opts: Opts,
 }
 
 impl App for GUI{
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         CentralPanel::default().show(ctx,|ui| {
-            ui.label("Test");
-            ui.label( self.lua_path.to_str().unwrap() );
-            if ui.button("...").clicked(){
-                self.lua_path = FileDialog::new().pick_file().expect("");
-            };
+            let grid = egui::Grid::new("id");
+            grid.show(ui, |ui|{
+                ui.label("codigo:");
+                ui.label( self.lua_path.to_str().unwrap() );
+                if ui.button("...").clicked(){
+                    match FileDialog::new().pick_file(){
+                        Some(v) => self.lua_path = v,
+                        _ => {},
+                    }
+                };
+                ui.end_row();
+
+                ui.label("baraja:");
+                ui.label( self.opts.deck.to_str().unwrap() );
+                if ui.button("...").clicked(){
+                    match FileDialog::new().pick_folder(){
+                        Some(v) => self.opts.deck = v,
+                        _ => {},
+                    }
+                };
+                ui.end_row();
+
+                ui.label("cartas:");
+                ui.label( self.opts.output.to_str().unwrap() );
+                if ui.button("...").clicked(){
+                    match FileDialog::new().pick_folder(){
+                        Some(v) => self.opts.output= v,
+                        _ => {},
+                    }
+                };
+
+                ui.end_row();
+
+                ui.label("nombre de baraja:");
+                ui.text_edit_singleline( &mut self.opts.in_format );
+                ui.end_row();
+
+                ui.label("nombre de carta:");
+                ui.text_edit_singleline( &mut self.opts.out_format );
+                ui.end_row();
+
+                if ui.button("run").clicked(){
+                    let opts = self.opts.clone();
+                    match Cli::config(opts, self.lua_path.clone()).and_then(Cli::run){
+                        Err(e) => {
+                            println!("{e}");
+                            self.error = format!("error: {e}");
+                        },
+                        _ => self.error = String::from("no errors!"),
+                    }
+                }
+            });
+
+
             ui.label( &self.error );
         });
     }
 }
 
 impl GUI {
-    pub fn new(opts: DataOpts) -> Self{
-        GUI{opts, ..Default::default()}
+    pub fn new(opts: Opts) -> Self{
+        let new = GUI{opts, ..Default::default()};
+
+        return new;
     }
 
-    pub fn run(mut self) -> anyhow::Result<()>{
+    pub fn run(self) -> anyhow::Result<()>{
         let options = eframe::NativeOptions{
             viewport: egui::ViewportBuilder::default()
                 .with_inner_size([640.0, 240.0]),
